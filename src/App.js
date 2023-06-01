c80c488856c896aa26b388d9fa8e9ba1182a3878
fayssal <fayssalmechmeche.pro@gmail.com>
2023-06-01 17:24:37 +0200

    add back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./View/home/Home";
 import Reglage from "./View/setting/Reglage";
 
 import ColorPicker from "./View/game/ColorPicker";
 import Game from "./View/game/Game";
 
+import mqtt from "precompiled-mqtt";
+
+const mqttConnect = () => {
+  const client = mqtt.connect("mqtt://test.mosquitto.org:8080");
+  console.log(client);
+  return client;
+};
+
+const mqttSub = () => {
+  const client = mqttConnect();
+  client.subscribe("message", function (err) {
+    if (!err) {
+      client.publish("message", "Hello back");
+      const onMessage = (callBack) => {
+        client.on("message", (topic, message, packet) => {
+          callBack(JSON.parse(new TextDecoder("utf-8").decode(message)));
+        });
+      };
+    } else {
+      console.log(err);
+    }
+  });
+};
+
 function App() {
+  useEffect(() => {
+    mqttSub();
+  }, []);
+
   return (
     <div>
       <Routes>
